f1e69d3fb1bffa5deb86074b6f86b05c8935be34
Barış Güneri <barisgnr.tr@gmail.com>
2022-04-11 21:27:58 +0300

    Create VucutKitleIndeksiHesaplayanProgram.java

---
+++ VucutKitleIndeksiHesaplayanProgram.java
@@ -0,0 +1,18 @@
+package Giris;
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double boy, kilo, boyKiloIndeks;
+
+        System.out.println("Boyunuzu Girin; ");
+        boy = input.nextDouble();
+        System.out.println("Kilonuzu Giriniz: ");
+        kilo = input.nextDouble();
+
+        boyKiloIndeks = kilo / (boy * boy);
+
+        System.out.println("Boy Kilo İndeksiniz: " + boyKiloIndeks);
+    }
+}
