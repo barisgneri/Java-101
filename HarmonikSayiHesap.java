264daa7c69389ab93ea0c5c7a53e3104f59ecc5d
Barış Güneri <barisgnr.tr@gmail.com>
2022-05-14 20:50:05 +0300

    Create HarmonikSayiHesap.java

---
+++ HarmonikSayiHesap.java
@@ -0,0 +1,16 @@
+package Giris;
+import java.util.Scanner;
+public class HelloWorld {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("N Sayısını Giriniz: ");
+        int n = input.nextInt();
+        double result = 0.0;
+
+        for (double i = 1;i <= n; i++){
+            result += (1/i);
+        }
+        System.out.println(result);
+    }
+}
+
